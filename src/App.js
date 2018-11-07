97a6798f938a04506033a18c043f3c426b8bceac
Martin Klavon <me@martinklavon.com>
2018-11-07 23:16:19 +0000

    Extended list display code

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 
 const list = [
         {
+            listId: 0,
             title: 'React It!',
             url: 'http://test1.com',
             author: 'Bob Smith',
         },
         {
+            listId: 1,
             title: 'Reactify',
             url: 'http://test2.com',
             author: 'Bob Smith',
@@ -19,7 +21,12 @@ class App extends Component {
       return (
           <div className="App">
               {list.map(function (item) {
-                  return <div>{item.title}</div>;
+                  return (
+                      <div key={item.listId}>
+                          <span><a href={item.url}>{item.title}</a></span>
+                          <span>{item.author}</span>
+                      </div>
+                  );
               })}
           </div>
       );
