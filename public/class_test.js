432a52211f6cf9d91378ecb978143352b34ad8c1
Martin Klavon <me@martinklavon.com>
2018-11-08 23:39:31 +0000

    Added static method to Test class

--- public/class_test.js
+++ public/class_test.js
@@ -9,8 +9,13 @@ class Test {
         return this.forename + ' ' + this.surname;
     }
 
+    static sayHello() {
+        return 'Hello!';
+    }
+
 }
 
 const bob = new Test('Bob', 'Smith');
 
 console.log(bob.getName());
+console.log(Test.sayHello());
