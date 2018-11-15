d71b4d8f763af3ecfff148d9a3cfdf342faed0db
creativeuphoria <me@martinklavon.com>
2018-11-15 23:32:29 +0000

    Playing with state

--- src/App.js
+++ src/App.js
@@ -1,34 +1,46 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
 const list = [
-        {
-            listId: 0,
-            title: 'React It!',
-            url: 'http://test1.com',
-            author: 'Bob Smith',
-        },
-        {
-            listId: 1,
-            title: 'Reactify',
-            url: 'http://test2.com',
-            author: 'Bob Smith',
-        },
-    ];
+    {
+        listId: 0,
+        title: 'React It!',
+        url: 'http://test1.com',
+        author: 'Bobby Bobberton',
+    },
+    {
+        listId: 1,
+        title: 'Reactify',
+        url: 'http://test2.com',
+        author: 'Bob Smith',
+    },
+];
 
 class App extends Component {
-  render() {
-      return (
-          <div className="App">
-              {list.map(item =>
-                  <div key={item.listId}>
-                      <span><a href={item.url}>{item.title}</a></span>
-                      <span>{item.author}</span>
-                  </div>
-              )}
-          </div>
-      );
-  }
+
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+            list: list,
+        };
+
+    }
+
+    render() {
+        return (
+            <div className="App">
+                {this.state.list.map(item =>
+                    <div key={item.listId}>
+                        <span><a href={item.url}>{item.title}</a></span>,
+                        <span>{item.author}</span>
+                    </div>
+                )}
+            </div>
+        );
+    }
+
 }
 
 export default App;
